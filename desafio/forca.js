6206219fe0e9d56fd2aa4078b3f01c2df1321bd6
Eduardo <eduhmq@gmail.com>
2022-07-20 03:30:47 -0300

    Commit inicial para este projeto. Criado a lógica para verificar match das letras

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,16 +1,50 @@
-class Forca {
+let letras = [];
+let resposta = 'abacaxi';
 
-  chutar(letra) { }
+const respostaString = resposta.split("");
+
+
+let match = [];
+for (let size = 0; size < respostaString.length; size++) {
+  match[size]='-'
+}
+class Forca {
+  chutar(letra) { 
+    if(!letras.includes(letra)) {
+      letras.push(letra);
+      for (let index = 0; index < respostaString.length; index++) {
+        if (respostaString[index] == letra) {
+          match[index]=letra;
+        }
+        else { 
+      }
+      console.log(match);
+    }  
+    
+    }
+    
+
+  }
+
+
+
+
 
   buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
+    
+
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          
+          letrasChutadas: letras, // Deve conter todas as letras chutadas
+          vidas: vidas, // Quantidade de vidas restantes
+          palavra: match // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
+
+ 
+
 }
 
 module.exports = Forca;
